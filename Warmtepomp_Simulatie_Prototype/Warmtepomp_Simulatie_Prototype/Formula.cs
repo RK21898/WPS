7a49b9173cd3dbb8ac84ba4e2424f177d564b714
rowan <rowan.koenen@gmail.com>
2018-10-05 14:00:54 +0200

    Added more formulas

--- Warmtepomp_Simulatie_Prototype/Warmtepomp_Simulatie_Prototype/Formula.cs
+++ Warmtepomp_Simulatie_Prototype/Warmtepomp_Simulatie_Prototype/Formula.cs
@@ -8,13 +8,44 @@ namespace Warmtepomp_Simulatie_Prototype
 {
     public class Formula
     {
-        public decimal COP(decimal powerOutput, decimal addedPower)
+        RoomTypeInformation rti;
+
+        /// <summary>
+        /// Convert kiloWatthour to joules
+        /// </summary>
+        /// <param name="kWh"></param>
+        /// <returns>the equivalent of x kWh as y joules</returns>
+        public decimal WattToJoules(decimal watt)
+        => watt * 1000;
+
+        /// <summary>
+        /// Calculate the power requirement for a room using
+        /// length * width * height * specific power per room
+        /// </summary>
+        /// <returns>power in watts</returns>
+        public decimal PowerRequirementForRoom(decimal length, decimal width, decimal height, string isolationquality)
         {
-            decimal cop;
-            return cop = powerOutput / addedPower;
+            decimal capacity = length * width * height;
+
+            int watt;
+            rti.RoomTypeInfo.TryGetValue(isolationquality, out watt); //fetch from watt per roomtype table
+
+            return capacity * watt; 
         }
 
         /// <summary>
+        /// Calculate the COP coefficient of the heat pump
+        /// power output is the power from the source
+        /// added power is power generated from the compressor in the pump
+        /// </summary>
+        /// <param name="powerOutput"></param>
+        /// <param name="addedPower"></param>
+        /// <returns>COP coefficient</returns>
+        public decimal COP(decimal powerOutput, decimal addedPower)
+        => powerOutput / addedPower;
+
+        /// <summary>
+        /// Calculates the volume flow rate
         /// Use for both Intake and Output sides in heat pump.
         /// power in Watts
         /// Density in kg/m^3
@@ -27,12 +58,10 @@ namespace Warmtepomp_Simulatie_Prototype
         /// <param name="deltaT"></param>
         /// <returns>volume flow rate in m^3/s</returns>
         public decimal WaterFlow(decimal power, decimal density, decimal specific_heat, decimal deltaT)
-        {
-            decimal qv;
-            return qv = power / (density * specific_heat * deltaT);
-        }
+        => power / (density * specific_heat * deltaT);
 
         /// <summary>
+        /// Seasonal Performance Factor
         /// Qw = Warmth delivered to building by heat pump in MWh
         /// Qk = Cold delivered to building by heat pump in MWh
         /// E = Electricity used by the pump system in MWh
@@ -43,10 +72,7 @@ namespace Warmtepomp_Simulatie_Prototype
         /// <param name="E"></param>
         /// <param name="G"></param>
         /// <returns>Seasonal Performance Factor</returns>
-        public decimal SeasonalPerformanceFactor(decimal Qw, decimal Qk, decimal E, decimal G)
-        {
-            decimal spf;
-            return spf = (Qw + Qk) / (E + G);
-        }
+        public decimal SPF(decimal Qw, decimal Qk, decimal E, decimal G)
+        => (Qw + Qk) / (E + G);
     }
 }
---
+++ Warmtepomp_Simulatie_Prototype/Warmtepomp_Simulatie_Prototype/RoomTypeInformation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmtepomp_Simulatie_Prototype
+{
+    public class RoomTypeInformation
+    {
+        /// <summary>
+        /// watts per square meter used to warm a room of x m^3 at a standard
+        /// of 22 degrees prior to corrections
+        /// </summary>
+        public Dictionary<string, int> RoomTypeInfo = new Dictionary<string, int>()
+        {
+            {"Excellent", 50},
+            {"Goed", 60},
+            {"Normaal", 70},
+            {"Slecht", 80}
+        };
+    }
+}
