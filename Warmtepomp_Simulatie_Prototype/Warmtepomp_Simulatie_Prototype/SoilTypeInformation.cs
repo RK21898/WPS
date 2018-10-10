89afd93a222ec2b99858ba0e7f58dcc751dc6d4f
RK21898 <rowan.koenen@gmail.com>
2018-10-10 17:46:18 +0200

    commit

---
+++ Warmtepomp_Simulatie_Prototype/Warmtepomp_Simulatie_Prototype/SoilTypeInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmtepomp_Simulatie_Prototype
+{
+    public class SoilTypeInformation
+    {
+        /// <summary>
+        /// watts per square meter collected by horizontal collector
+        /// </summary>
+        public Dictionary<string, int> HorizontalCollectorInfo = new Dictionary<string, int>()
+        {
+            {"Droge zanderige bodem", 50},
+            {"Goed", 60},
+            {"Normaal", 70},
+            {"Slecht", 80}
+        };
+
+        /// <summary>
+        /// watts per meter collected by vertical collector
+        /// </summary>
+        public Dictionary<string, int> VerticalCollectorInfo = new Dictionary<string, int>()
+        {
+            {"Droog sediment", 50},
+            {"Goed", 60},
+            {"Normaal", 70},
+            {"Slecht", 80}
+        }; 
+
+        // finish inputting data into these dictionaries
+    }
+}
