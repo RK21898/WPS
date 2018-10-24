7b49bc80f5445bf4c94a249d68b9ee01089ec33a
Bram Linssen <BLinssen95@Gmail.com>
2018-10-24 10:33:33 +0200

    Converted C# formules to Py

---
+++ Formules.py
@@ -0,0 +1,22 @@
+
+#Watt To Joules function
+def WattToJoules(watt):
+    return watt * 1000
+
+def PowerRequirementForRoom(length, width, height, watt):
+    return capacity = length * width * height * watt
+
+def COP(powerOutput, addedPower):
+    return powerOutput / addedPower
+
+def WaterFlow(power, density, specific_heat, deltaT):
+    return power / (density * specific_heat * deltaT)
+
+def SPF(Qw, Qk, E, G):
+    return (Qw + Qk) / (E + G)
+
+def PowerRequirementToHeatUpSubstance(m, c, DeltaT):
+    return (m * c * DeltaT) / 3600000
+
+def TimeRequirementToHeatUpSubstance(power, requirement):
+    return (requirement / power) * 3600
