1c032682cd58f399166c2f44351f35f7e6648647
RK21898 <rowan.koenen@gmail.com>
2018-10-26 10:52:37 +0200

    broem

--- dataport.py
+++ dataport.py
@@ -2,16 +2,16 @@
 import csv
 
 def importCSV(path):
-    with open(path, 'rb') as csvfile:
-        valReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    csvData = []    
-    for row in valReader:
-        csvData.append(row)
+    with open(path, 'r', encoding="utf-8-sig") as csvfile:
+        valReader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        csvData = [[]]    
+        for row in valReader:
+            csvData.append(row)
     return csvData
 
 def exportCSV(file, path):
     with open(path, 'wb') as csvfile:
-        csvWriter = csv.writer(csvfile, delimiter=' ',
+        csvWriter = csv.writer(csvfile, delimiter=';',
             quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for row in file:
             csvWriter.writerow(row)
--- plot.py
+++ plot.py
@@ -1,15 +1,15 @@
-import numpy
 import matplotlib.pyplot as mpl
 import tempfile as tmp
 import dataport as dpt
+import os 
 
 tempdir = tmp.gettempdir()
 
 def ShowGraph(filename):
-    data = dpt.importCSV('..//'+filename+'.csv')
+    data = dpt.importCSV(os.path.abspath(".\\"+filename+".csv"))
     mpl.plot(data)
-    mpl.ylabel('Tijd')
-    mpl.xlabel('Data')
+    mpl.ylabel("Tijd")
+    mpl.xlabel("Data")
     mpl.show()
 
-ShowGraph('WPS')
+ShowGraph("dummydata")
